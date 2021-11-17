e4695f9b1e6fcfcdb9b881c9023eea994ae8050b
Deknoinarak <60281096+Deknoinarak@users.noreply.github.com>
2021-11-17 20:49:07 +0700

    Fixed .htaccess and web.config to display index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     require_once "/../inetpub/github/deknoinarak-management-system/includes/navbar.php";
 
     // Switch page by index.php?page=home or index/home
-    if ($_GET['page']) {
+    if (isset($_GET['page'])) {
         // Copy and paste if statement for add page and change 'home' to your page and your page file e.g. /../inetpub/cms/deknoinarak-cms-core/includes/pages/home.php
         if ($_GET['page'] == 'home') {
             include('/../inetpub/github/deknoinarak-management-system/includes/pages/home.php');
@@ -15,6 +15,12 @@
         if ($_GET['page'] == 'aboutus') {
             include('/../inetpub/github/deknoinarak-management-system/includes/pages/aboutus.php');
         }
+        else {
+            include('/../inetpub/github/deknoinarak-management-system/includes/error/404.php');
+        }
+    }
+    else {
+        header("Location: index/home");
     }
 
     // Includes Footer
